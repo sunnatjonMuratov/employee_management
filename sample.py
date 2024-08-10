1380d4a818a379c4a9feef5f98c45d9ecd78366e
Sunnatjon Muratov <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-08-10 12:23:49 +0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,14 +5,13 @@ sample=f'''\tCompany Name, XYZ\n\tAddress: Xyz, Floor4
     Generated On\t\t: 20-12-2020
     
 ...................................................
-    
     Total Days\t\t: 31
     Total Present\t\t: 20
     Convinence\t\t: Rs.1000
     Medical\t\t: Rs.2000
     PF\t\t: Rs.1500
+    
 ...................................................
-
     Net Salary\t\t: Rs.25455222
     This is computer generated slip, not required any signature
 '''
